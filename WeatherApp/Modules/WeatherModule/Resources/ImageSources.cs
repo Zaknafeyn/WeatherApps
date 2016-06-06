9e5d374e3d75aec8b9899f5cc820ea92062b6e2f
Valentie Radchuk <vradchuk@cogniance.com>
2016-06-06 18:01:28 +0300

    Add icons and show icons for appropriate weather

---
+++ WeatherApp/Modules/WeatherModule/Resources/ImageSources.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace WeatherModule.Resources
+{
+    public class ImageSources
+    {
+        private const string ModuleName = "WeatherModule";
+
+        public static readonly Uri BrightMoonImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Bright Moon.png");
+        public static readonly Uri ChanceofStormImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Chance of Storm.png");
+        public static readonly Uri CloudLightingImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Cloud Lighting.png");
+        public static readonly Uri CloudsImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Clouds.png");
+        public static readonly Uri DownpourImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Downpour.png");
+        public static readonly Uri DryImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Dry.png");
+        public static readonly Uri DustImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Dust.png");
+        public static readonly Uri FogDayImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Fog Day.png");
+        public static readonly Uri FogNightImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Fog Night.png");
+        public static readonly Uri HailImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Hail.png");
+        public static readonly Uri HeavyRainImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Heavy Rain.png");
+        public static readonly Uri HighTideImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/High Tide.png");
+        public static readonly Uri IcyImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Icy.png");
+        public static readonly Uri IntenseRainImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Intense Rain.png");
+        public static readonly Uri LightRain2Image = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Light Rain 2.png");
+        public static readonly Uri LightRainImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Light Rain.png");
+        public static readonly Uri LightSnowImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Light Snow.png");
+        public static readonly Uri LightningBoltImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Lightning Bolt.png");
+        public static readonly Uri LowTideImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Low Tide.png");
+        public static readonly Uri ModerateRainImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Moderate Rain.png");
+        public static readonly Uri MoistureImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Moisture.png");
+        public static readonly Uri NoRainImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/No Rain.png");
+        public static readonly Uri PartlyCloudyDayImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Partly Cloudy Day.png");
+        public static readonly Uri PartlyCloudyNightImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Partly Cloudy Night.png");
+        public static readonly Uri PartlyCloudyRainImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Partly Cloudy Rain.png");
+        public static readonly Uri RainImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Rain.png");
+        public static readonly Uri RainyWeatherImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Rainy Weather.png");
+        public static readonly Uri SleetImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Sleet.png");
+        public static readonly Uri SnowStormImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Snow Storm.png");
+        public static readonly Uri SnowImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Snow.png");
+        public static readonly Uri StormImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Storm.png");
+        public static readonly Uri SunImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Sun.png");
+        public static readonly Uri SunriseImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Sunrise.png");
+        public static readonly Uri SunsetImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Sunset.png");
+        public static readonly Uri TorrentialRainImage = new Uri($"pack://application:,,,/{ModuleName};component/Resources/Icons/Weather/Torrential Rain.png");
+
+
+    }
+}
--- WeatherApp/Services/RecentCities.cs
+++ WeatherApp/Services/RecentCities.cs
@@ -2,8 +2,9 @@
 
 namespace Services
 {
-    public class RecentCities 
+    public class RecentCities
     {
+        private const int MaxCount = 5;
         public List<string> Cities { get; } = new List<string>();
 
         public void AddCity(string city)
@@ -11,6 +12,12 @@ namespace Services
             if (Cities.Contains(city))
             {
                 Cities.Remove(city);
+            }
+
+
+            while (Cities.Count >= MaxCount)
+            {
+                Cities.RemoveAt(0);
             }
 
             Cities.Add(city);
